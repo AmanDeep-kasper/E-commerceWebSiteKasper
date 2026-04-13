5c3bdade83816c557995ae15209b90a2f3978bbf
moid-kasperinfotech <moid.alam@kasperinfotech.com>
2026-04-14 01:19:25 +0530

    cors issue fix

--- server/src/app.js
+++ server/src/app.js
@@ -40,6 +40,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173", // 🔥 ADD THIS
   "http://localhost:5174",
+  "https://e-commercewebsitekasper.onrender.com",
 ];
 
 const corsOptions = {
