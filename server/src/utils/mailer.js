330e5d8ea7bf56b0efeadde5e3e74875d5592c4c
moid-kasperinfotech <moid.alam@kasperinfotech.com>
2026-04-14 00:22:35 +0530

    .email fix 2

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -2,20 +2,21 @@ import nodemailer from "nodemailer";
 import env from "../config/env.js";
 
 const emailConfig = {
-  host: env.SMTP_HOST,
-  port: env.NODE_ENV === "development" ? 587 : 465,
-  secure: env.NODE_ENV !== "development", // true for 465, false for other ports
+  // host: env.SMTP_HOST,
+  service: "gmail",
+  // port: env.NODE_ENV === "development" ? 587 : 465,
+  // secure: env.NODE_ENV !== "development", // true for 465, false for other ports
   auth: {
     user: env.SMTP_USER,
     pass: env.SMTP_PASSWORD,
   },
-  pool: true,
-  maxConnections: env.SMTP_MAX_CONNECTIONS,
-  maxMessages: env.SMTP_MAX_MESSAGES,
-  rateLimit: env.SMTP_RATE_LIMIT,
-  connectionTimeout: env.SMTP_CONNECTION_TIMEOUT,
-  greetingTimeout: env.SMTP_GREETING_TIMEOUT,
-  socketTimeout: env.SMTP_SOCKET_TIMEOUT,
+  // pool: true,
+  // maxConnections: env.SMTP_MAX_CONNECTIONS,
+  // maxMessages: env.SMTP_MAX_MESSAGES,
+  // rateLimit: env.SMTP_RATE_LIMIT,
+  // connectionTimeout: env.SMTP_CONNECTION_TIMEOUT,
+  // greetingTimeout: env.SMTP_GREETING_TIMEOUT,
+  // socketTimeout: env.SMTP_SOCKET_TIMEOUT,
 };
 
 let transporter = null;
