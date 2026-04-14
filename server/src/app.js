93fbd64daa487eb4b97fce02e48fdf370112f25f
Akash Kumar <userakashnettechinnvo123@gmail.com>
2026-04-14 17:23:05 +0530

    modified

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,8 @@
 // import dotenv from "dotenv";
 // dotenv.config();
 
-// import dns from "node:dns";
-// dns.setServers(["1.1.1.1", "1.0.0.1", "8.8.8.8", "8.8.4.4"]);
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "1.0.0.1", "8.8.8.8", "8.8.4.4"]);
 
 import express from "express";
 import cors from "cors";
