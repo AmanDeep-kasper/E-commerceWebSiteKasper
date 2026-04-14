a98c307ca0c6a5ab9811444e8f31e4371c937e90
moid-kasperinfotech <moid.alam@kasperinfotech.com>
2026-04-14 12:32:54 +0530

    mail test localhost

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const getTransporter = () => {
-  const user = process.env.EMAIL_USER;
-  const pass = process.env.EMAIL_PASS;
+  const user = process.env.SMTP_USER;
+  const pass = process.env.SMTP_PASSWORD;
 
   if (!user || !pass) throw new Error("EMAIL_USER or EMAIL_PASS is missing");
 
   const transporter = nodemailer.createTransport({
-    host: env.SMTP_HOST,
-    port: env.NODE_ENV === "production" ? 465 : 587,
-    secure: env.NODE_ENV === "production" ? true : false,
+    host: process.env.SMTP_HOST,
+    port: process.env.NODE_ENV === "production" ? 465 : 587,
+    secure: process.env.NODE_ENV === "production" ? true : false,
 
     auth: {
       user,
